8ad2b489368e390af8c1b575c786d3b13fefacff
unknown <gonzalo.n.delia@gmail.com>
2018-11-04 23:36:00 -0300

    updated starting point for server - was not working properly

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+<<<<<<< Updated upstream
+=======
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const storage = require('./hostStorage');
+
+app.use(fileUpload());
+app.use(cors());
+
+const backendPort = 8001;
+
+app.post('/upload', async (req, res) => {
+    try {
+        await storage.store(req.files.file);
+        res.sendStatus(200);
+    } catch(err) {
+        res.sendStatus(500);
+    }
+});
+
+http.listen(backendPort);
+>>>>>>> Stashed changes
--- frontend/server.js
+++ frontend/server.js
@@ -1,15 +1,16 @@
 let http = require('http');
+const url = require('url');
 let fs = require('fs');
 
-const PORT=8000; 
+const PORT=8000;
 
-fs.readFile('./index.html', (err, html) => {
-
-    if (err) throw err;    
-
-    http.createServer((request, response) => {  
-        response.writeHeader(200, {'Content-Type': 'text/html'});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
-});
+http.createServer((req, res) => {
+    let requestUrl = url.parse(req.url);
+    res.writeHead(200);
+    let stream = fs.createReadStream(__dirname + (requestUrl.pathname === '/' ? '/index.html' : requestUrl.pathname));
+    stream.pipe(res);
+    stream.on('error', () => {
+        res.writeHead(404);
+        res.end();
+    })
+}).listen(PORT);
